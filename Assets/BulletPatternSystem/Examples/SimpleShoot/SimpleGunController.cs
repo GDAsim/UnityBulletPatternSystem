269058e8fbbdbac261c42d0a4f3b20554a18cfc8
Khiew Jian Bin <khiewbin@gmail.com>
2025-02-15 10:08:47 +0800

    refractor to sync with ECS

--- Assets/BulletPatternSystem/Examples/SimpleShoot/SimpleGunController.cs
+++ Assets/BulletPatternSystem/Examples/SimpleShoot/SimpleGunController.cs
@@ -1,13 +1,33 @@
+using System;
 using UnityEngine;
 
 public class SimpleGunController : MonoBehaviour
 {
     [SerializeField] GunStats baseStats;
     [SerializeField] Gun gun;
+    [SerializeField] GunPatternSelect PatternSelect;
+
+    enum GunPatternSelect
+    {
+        Straight,
+        Sine
+    }
 
     void Awake()
     {
-        var bulletPattern = BulletPatterns.Straight(baseStats.Power);
+        IAction[] bulletPattern;
+        switch (PatternSelect)
+        {
+            case GunPatternSelect.Straight:
+                bulletPattern = BulletPatterns.Straight(baseStats.Power);
+                break;
+            case GunPatternSelect.Sine:
+                bulletPattern = BulletPatterns.Sine(baseStats.Power, Vector3.left, 1f);
+                break;
+            default:
+                throw new NotImplementedException();
+        }
+        
         var stats = Instantiate(baseStats);
         gun.SetupShoot(bulletPattern, stats);
     }
